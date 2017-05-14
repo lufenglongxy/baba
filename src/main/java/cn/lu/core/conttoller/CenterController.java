42a069ebfa1fb710eafe080752ed37758480f573
lu <eacogo@163.com>
2017-05-14 21:37:31 +0800

    first

---
+++ src/main/java/cn/lu/core/conttoller/CenterController.java
@@ -0,0 +1,11 @@
+package cn.lu.core.conttoller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by lufenglong on 2017/5/14.
+ */
+
+@Controller
+public class CenterController {
+}
--- src/main/java/test.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by lufenglong on 2017/5/14.
- */
-public class test {
-}
