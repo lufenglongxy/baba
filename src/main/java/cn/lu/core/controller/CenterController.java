858b283c2ca4defb0db7569cc2d59b071d6bdd5c
lu <eacogo@163.com>
2017-05-17 20:53:27 +0800

    first

--- src/main/java/cn/lu/core/controller/CenterController.java
+++ src/main/java/cn/lu/core/controller/CenterController.java
@@ -1,11 +1,33 @@
 package cn.lu.core.controller;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.support.WebBindingInitializer;
+import org.springframework.web.context.request.WebRequest;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by lufenglong on 2017/5/14.
  */
 
 @Controller
-public class CenterController {
+public class CenterController implements WebBindingInitializer{
+
+    @RequestMapping(value="/test/springmvc.do")
+    public String test(String name ,Date birthday){
+        System.out.println(name+"----"+birthday );
+        return "";
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder, WebRequest webRequest) {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        webDataBinder.registerCustomEditor(Date.class,new CustomDateEditor(dateFormat,true));
+    }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,7 +1,17 @@
-<html>
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>Title</title>
+</head>
 <body>
 <h2>Hello World!</h2>
 
 <h2>Hello World!</h2>
+<form action="/test/springmvc.do" method="post">
+    <input type="text" value="2017-01-01 01:01:01" name="birthday">
+    <input type="submit" value="提交">
+</form>
 </body>
 </html>
